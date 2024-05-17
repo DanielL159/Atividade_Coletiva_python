c1d0a307e239af3682e69d915cc7bd2d14be3fff
Pedro Chaves <107230091+Pedro-Chaves2505@users.noreply.github.com>
2024-05-17 18:55:14 -0300

    Update usuario.py

--- usuario.py
+++ usuario.py
@@ -1,31 +1,31 @@
 class Usuario:
-    __slots__ = ['_nome', '_agenda']
-    def __init__(self,nome,agenda):
-        self._nome = nome
-        self._agenda=agenda
-        
-    @property   
-    def nome(self):
-        return self._nome
+  __slots__ = ['_nome', '_agenda']
+  def __init__(self,nome,agenda):
+      self._nome = nome
+      self._agenda=agenda
 
-    @property
-    def agenda(self):
-        return self._agenda
-    
-    def adicionarTarefa(self,tarefa):
-        self.agenda.adicionarALista(tarefa)
-        
-    def removerTarefa(self,nome):
-        posicao=-1
-        for variavel in self.agenda.lista:
-            posicao+=1
-            if variavel.nome == nome.upper():
-                self.agenda.removerDaLista(posicao)
-                break;
-        
-    def mostraAtividade(self):
-        self.agenda.mostraAtividade()
+  @property   
+  def nome(self):
+      return self._nome
+
+  @property
+  def agenda(self):
+      return self._agenda
+
+  def adicionarTarefa(self,tarefa):
+      self.agenda.adicionarALista(tarefa)
+
+  def removerTarefa(self,nome):
+      posicao=-1
+      for variavel in self.agenda.lista:
+          posicao+=1
+          if variavel.nome == nome.upper():
+              self.agenda.removerDaLista(posicao)
+              break;
+
+  def mostraAtividade(self):
+      self.agenda.mostraAtividade()
 
 
-    def __str__(self):
-        return f"{self.nome} :\n {self.agenda}"
+  def __str__(self):
+      return f"{self.nome} :\n {self.agenda}"
